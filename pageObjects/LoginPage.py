c90258effaa35ec2cc7365538ca6a6df1a86bf16
ankramani <ankit.ramani1@gmail.com>
2021-07-01 13:13:39 +0530

    project ready

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 class Login:
     txtbox_username_id = "Email"
     txtbox_password_id = "Password"
-    button_login_class = "button-1 login-button"
+    button_login_class = '//button[@class="button-1 login-button"]'
     link_logout_linktext = "Logout"
 
     def __init__(self, driver):
@@ -18,7 +18,7 @@ class Login:
         self.driver.find_element_by_id(self.txtbox_password_id).send_keys(password)
 
     def clickLogin(self):
-        self.driver.find_element_by_class_name(self.button_login_class).click()
+        self.driver.find_element_by_xpath(self.button_login_class).click()
 
     def clickLogout(self):
         self.driver.find_element_by_link_text(self.link_logout_linktext).click()
--- testCases/test_login.py
+++ testCases/test_login.py
@@ -9,3 +9,14 @@ class Test_001_Login:
 
     def test_homePageTitle(self, setup):
         self.driver = setup
+        self.driver.get(self.baseURL)
+        self.login = Login(self.driver)
+        self.login.setUsername(self.username)
+        self.login.setPassword(self.password)
+        self.login.clickLogin()
+        act_title = self.driver.title
+        if act_title == "Dashboard / nopCommerce administration":
+            assert True
+        else:
+            assert False
+        self.driver.close()
